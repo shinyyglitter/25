203611c493cf2b7a5006072bde459feba1ac6c41
shinyyglitter <sunnknu0@gmail.com>
2025-01-15 10:34:31 +0100

    C:/Users/sunnk/AppData/Local/Temp/poem skriver nå ut et dikt

--- script.mjs
+++ script.mjs
@@ -7,11 +7,14 @@ const port = (process.env.PORT || 8000);
 server.set('port', port);
 server.use(express.static('public'));
 
-function getRoot(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
-}
+const dikt = "Jeg våknet en natt av en underlig drøm, det var som en stemme talte til meg, fjern som en underjordisk strøm-, og jeg reiste meg opp: Hva er det du vil meg?,– Du må ikke sove! Du må ikke sove. Du må ikke tro at du bare har drømt, I går ble jeg dømt, I natt reiste de skafottet i gården, De henter meg klokken fem i morgen"
 
-server.get("/", getRoot);
+const listDikt = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send(dikt).end();}
+/*function getRoot(req, res, next) {
+    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
+}*/
+
+server.get("/", listDikt);
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
