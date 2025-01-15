aaf185b1cce6e770312786ff067751e90dba759e
shinyyglitter <sunnknu0@gmail.com>
2025-01-15 18:18:57 +0100

    Serveren kan nå regne sammen summen av to tall

--- script.mjs
+++ script.mjs
@@ -55,6 +55,26 @@ const ListRandomQuote = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).s
 
 server.get("/tmp/quote", ListRandomQuote)
 
+//---------Regne ut sum---------
+const ListSum = (req, res) => {
+    const a = parseFloat(req.params.a);
+    const b = parseFloat(req.params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+      return res.status(HTTP_CODES.CLIENT_ERROR.BAD_REQUEST).send("Bad Request: 'a' og 'b' må være gyldige tall.");
+    }
+  
+    const sum = a + b;
+  
+    res.status(HTTP_CODES.SUCCESS.OK).send(`Summen av ${a} og ${b} er ${sum}`);
+  };
+  
+
+
+
+server.post('/tmp/sum/:a/:b', ListSum)
+server.get('/tmp/sum/:a/:b', ListSum)
+
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
