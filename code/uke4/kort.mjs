773dac9443d2cffedf635406f516fa50f4745c6b
shinyyglitter <sunnknu0@gmail.com>
2025-01-22 15:15:58 +0100

    Kan nå genrere en unik ID og kortstokk

---
+++ code/uke4/kort.mjs
@@ -0,0 +1,17 @@
+import HTTP_CODES from "../../utils/httpCodes.mjs";
+
+const decks = {};
+const deck_id = Math.random().toString(36).substring(2,8);
+
+const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const values = ['2', '3', '4','5','6','7','8','9','10','Jack','Queen', 'King','Ace'];
+
+const cards = [];
+
+for(const suit of suits){
+    for(const value of values){
+        cards.push({suit, value});
+    }
+}
+decks[deck_id] = {cards, drawn:[]}
+export const MakeDeck = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send({deck_id}).end();}
--- script.mjs
+++ script.mjs
@@ -3,6 +3,7 @@ import HTTP_CODES from './utils/httpCodes.mjs';
 import { randomquote } from './code/uke3/quote.mjs';
 import { poem } from './code/uke3/poem.mjs';
 import { ListSum } from './code/uke3/sum.mjs';
+import { MakeDeck } from './code/uke4/kort.mjs';
 
 const server = express();
 const port = (process.env.PORT || 8000);
@@ -10,6 +11,9 @@ const port = (process.env.PORT || 8000);
 server.set('port', port);
 server.use(express.static('public'));
 
+//--------------------------------
+//UKE 3
+//--------------------------------
 
 //-----------Poem-----------
 const listPoem = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();}
@@ -17,11 +21,28 @@ server.get("/tmp/poem", listPoem);
 
 //----------Quote--------------
 const ListRandomQuote = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send(randomquote).end();}
-server.get("/tmp/quote", ListRandomQuote)
+server.get("/tmp/quote", ListRandomQuote);
 
 //---------Sum---------
-server.post('/tmp/sum/:a/:b', ListSum)
+server.post('/tmp/sum/:a/:b', ListSum);
 
+
+//--------------------------------
+//UKE 4
+//--------------------------------
+
+//---------Oprett kortstokk------
+
+server.get('/tmp/deck', MakeDeck);
+
+/*//--------Stokk Kortstokken------
+server.patch('/tmp/deck/shuffle/:deck_id', ShuffleDeck);
+
+//-----Returnerer kortstokken----
+server.get('/tmp/deck/:deck_id', ShowDeck);
+
+//--Vis tilfeldig kort fra bunken--
+server.get('/tmp/deck/:deck_id/card', ShowCard);*/
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
