df16f06c08919d528e7610a3097657b492f28b1b
shinyyglitter <sunnknu0@gmail.com>
2025-01-24 14:37:59 +0100

    lagt til flere feilkoder

--- utils/httpCodes.mjs
+++ utils/httpCodes.mjs
@@ -2,12 +2,24 @@ import { setUncaughtExceptionCaptureCallback } from "node:process"
 
 const HTTP_CODES = {
 
+    INFO: {
+        CONTINUE: 100,
+        SWITCHING_PROTOCOLS: 101,
+        PROCESSING: 102,
+        EARLY_HINTS: 103
+    },
     SUCCESS: {
-        OK: 200
+        OK: 200,
+        CREATED: 201,
+        ACCEPTED: 202
     },
     CLIENT_ERROR: {
         NOT_FOUND: 404,
-        BAD_REQUEST: 400
+        BAD_REQUEST: 400,
+        IM_A_TEAPOT: 418
+    },
+    SERVER_ERROR:{
+        INTERNAL_SERVER_ERROR: 500,        
     }
 }
 
