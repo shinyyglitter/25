eebcc568da7ed635bab968876e905cf4f2924e29
shinyyglitter <sunnknu0@gmail.com>
2025-02-20 12:47:04 +0100

    manglet kode fra timen

---
+++ data/tree.mjs
@@ -0,0 +1,18 @@
+const Tree = function (root) {
+    return { root };
+}
+
+const Node = function (data, ...connections) {
+    return { data, connections: [...connections] }
+}
+
+export function saveTree(tree) {
+    return JSON.stringify(tree, null, 3);
+}
+
+export function inflateTree(data) {
+    return JSON.parse(data);
+}
+
+
+export { Tree, Node };
