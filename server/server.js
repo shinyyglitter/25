cf808b5a31d6950b9f214390ab3a1802f837a71b
shinyyglitter <sunnknu0@gmail.com>
2025-03-03 16:52:22 +0100

    enda mer rydding og oppdatert forside

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import HTTP_CODES from './utils/httpCodes.mjs';
-import { randomquote } from '../code/uke3/quote.mjs';
-import { poem } from '../code/uke3/poem.mjs';
-import { ListSum } from '../code/uke3/sum.mjs';
-import { MakeDeck, ShuffleDeck, ShowDeck, DrawCard } from '../code/uke4/kort.mjs';
+
 import log from '../modules/log.mjs';
 import { LOGG_LEVELS } from '../modules/log.mjs';
 import abTest from '../modules/abtesting.mjs';
@@ -32,38 +29,7 @@ server.use("/graph/", graphRouter);
 server.use("/quest", questLogRouter);
 server.use("/user", userRouter);
 
-//--------------------------------
-//UKE 3
-//--------------------------------
 
-//-----------Poem-----------
-const listPoem = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();}
-server.get("/tmp/poem", listPoem);
-
-//----------Quote--------------
-const ListRandomQuote = (req, res, next) => {res.status(HTTP_CODES.SUCCESS.OK).send(randomquote).end();}
-server.get("/tmp/quote", ListRandomQuote);
-
-//---------Sum---------
-server.post('/tmp/sum/:a/:b', ListSum);
-
-
-//--------------------------------
-//UKE 4
-//--------------------------------
-
-//---------Oprett kortstokk------
-server.post('/tmp/deck', MakeDeck);
-   
-//--------Stokk Kortstokken------
-// Ensure that you handle the shuffle route correctly
-server.patch('/tmp/deck/shuffle/:deck_id', ShuffleDeck);
-
-//-----Returnerer kortstokken----
-server.get('/tmp/deck/:deck_id', ShowDeck);
-
-//--Vis tilfeldig kort fra bunken--
-server.get('/tmp/deck/:deck_id/card', DrawCard);
 
 //--------------------------------
 //UKE 6
