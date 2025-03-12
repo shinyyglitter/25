99c79ebc09557627cc9246baf8c7edf0ff8dde7a
shinyyglitter <sunnknu0@gmail.com>
2025-03-12 19:59:54 +0100

    små endringer

--- data/userRecordStore.mjs
+++ data/userRecordStore.mjs
@@ -1,32 +1,34 @@
 import RecordStoreAbstractInterface from "./abstractRecordStore.mjs";
 import DbManager from "./db.js";
 
-
-class UserRecordStore extends RecordStoreAbstractInterface{
+class UserRecordStore extends RecordStoreAbstractInterface {
     create(user) {
-        DbManager.create(`INSERT INTO "public"."users"("username") VALUES($1, $2) RETURNING "user_id", "username";`, user)
-    }
-
-    read(user){
-        return DbManager.read(
-            `SELECT "user_id", "username" FROM "public"."users" WHERE "user_id" = $1;`, 
-            [user.user_id]
+        return DbManager.create(
+            `INSERT INTO "public"."users"("username") VALUES($1) RETURNING "user_id", "username";`, 
+            [user.username]
         );
     }
 
-    update(user){
+    read(user_id) {
+        return DbManager.read(
+            `SELECT "user_id", "username" FROM "public"."users" WHERE "user_id" = $1;`, 
+            [user_id]
+        );
+    }
+
+    update(user) {
         return DbManager.update(
             `UPDATE "public"."users" SET "username" = $1 WHERE "user_id" = $2 RETURNING "user_id", "username";`, 
             [user.username, user.user_id]
         );
     }
 
-    remove(user){
+    remove(user_id) {
         return DbManager.remove(
             `DELETE FROM "public"."users" WHERE "user_id" = $1 RETURNING "user_id";`, 
-            [user.user_id]
+            [user_id]
         );
     }
 }
 
-export default UserRecordStore
+export default UserRecordStore;
