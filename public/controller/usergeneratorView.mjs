b45aedb73398d8b59123c8d8769cd698daba08c9
shinyyglitter <sunnknu0@gmail.com>
2025-03-12 19:50:01 +0100

    lagt til knapp for å lage bruker

--- public/controller/usergeneratorView.mjs
+++ public/controller/usergeneratorView.mjs
@@ -1,10 +1,12 @@
 import TemplateManager from "../modules/templateManager.mjs";
+import { createUser } from "../../modules/generateUser.mjs";
 const templateFile = "usergeneratorView.html";
 
 const template = TemplateManager.fetchTemplate(templateFile);
 const usergeneratorView = TemplateManager.cloneTemplate(template, document.body);
 
 loginView.getElementById("button").onclick = (evt) => {
+    createUser()
     console.log("knappen funker");
 }
 
